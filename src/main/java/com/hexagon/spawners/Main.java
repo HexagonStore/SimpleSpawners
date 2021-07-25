09378a2553a63700840767f42f3eecf05312c2f9
Tiago Dinis <tiagodinis33@gmail.com>
2021-07-25 16:15:19 +0100

    added getlore and getSpawnerName

--- src/main/java/com/hexagon/spawners/Main.java
+++ src/main/java/com/hexagon/spawners/Main.java
@@ -2,6 +2,10 @@ package com.hexagon.spawners;
 
 
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 
 public class Main extends JavaPlugin
@@ -11,6 +15,18 @@ public class Main extends JavaPlugin
     public void onEnable(){
         this.config = new BukkitConfig("spawners.yml", this);
         
-        Bukkit.getConsoleSender().sendMessage("[HexagonSpawners] Plugin iniciado com sucesso!");
+        Bukkit.getConsoleSender().sendMessage("Plugin iniciado com sucesso!");
+    }
+    public List<String> getLore(String mob){
+        ArrayList<String> result = new ArrayList<>(config.getConfig().getStringList("spawner.lore"));
+        int i = 0;
+        for(String line : result){
+            result.set(i, line.replace("<mob>", mob));
+            i++;
+        }
+        return new ArrayList<>(result);
+    }
+    public String getSpawnerName(String mob){
+        return config.getConfig().getString("spawner.name").replace("<mob>", mob);
     }
 }
